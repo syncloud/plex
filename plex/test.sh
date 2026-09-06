bcd4c94c6c349d5f247949c258f67306c329e4c9
Boris Rybalkin <ribalkin@gmail.com>
2026-09-06 11:13:46 +0100

    plex: the transcoder is ffmpeg, so ask it for -version not --version
    "Plex Transcoder" --version prints the banner and then exits 8 with
    "Unrecognized option '-version'. Error splitting the argument list", failing
    plex test on every arch. ffmpeg takes a single dash.
    
    I had claimed this binary exited 0 when checking the layout change by hand. It
    did not; I read $? after a pipe, so I was measuring head's exit status rather
    than the binary's. Re-measured without the pipe: --version is 8, -version is 0.

--- plex/test.sh
+++ plex/test.sh
@@ -9,4 +9,4 @@ unset LD_LIBRARY_PATH
 
 "${PLEX_DIR}/Plex Media Server" --version
 "${PLEX_DIR}/Plex Tuner Service" --version
-"${PLEX_DIR}/Plex Transcoder" --version > /dev/null
+"${PLEX_DIR}/Plex Transcoder" -version > /dev/null
